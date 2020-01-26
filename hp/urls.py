c0da41bc47f5243f9cc4d7448b9acd63dbaf9411
Taichi Kanaya <taichi.kanaya@attakait.com>
2020-01-26 17:57:07 +0900

    フロントエンド大枠実装

--- hp/urls.py
+++ hp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from hp import views
-
+app_name='hp'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
-    path('top/', views.TopView.as_view(), name="top"),
+    path('home/', views.HomeView.as_view(), name="home"),
+    path('self_introduction/', views.SelfIntroductionView.as_view(), name="self_introduction"),
+    path('portfolios/', views.PortfoliosView.as_view(), name="portfolios"),
+    path('skills/', views.SkillsView.as_view(), name="skills"),
 ]
--- hp/views.py
+++ hp/views.py
@@ -5,5 +5,17 @@ class LoginView(generic.TemplateView):
     template_name = 'login.html'
 
 
-class TopView(generic.TemplateView):
-    template_name = 'top.html'
+class HomeView(generic.TemplateView):
+    template_name = 'home.html'
+
+
+class SelfIntroductionView(generic.TemplateView):
+    template_name = 'self_introduction.html'
+
+
+class PortfoliosView(generic.TemplateView):
+    template_name = 'portfolios.html'
+
+
+class SkillsView(generic.TemplateView):
+    template_name = 'skills.html'
