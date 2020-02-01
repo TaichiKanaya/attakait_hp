34700df63b58a6ba1f4d4bd9407880a25082b166
Taichi Kanaya <taichi.kanaya@attakait.com>
2020-02-01 22:54:42 +0900

    ホームページ、自己紹介ページ、お問い合わせページ作成

--- hp/urls.py
+++ hp/urls.py
@@ -5,7 +5,8 @@ app_name='hp'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
     path('home/', views.HomeView.as_view(), name="home"),
-    path('self_introduction/', views.SelfIntroductionView.as_view(), name="self_introduction"),
+    path('about_me/', views.AboutMeView.as_view(), name="about_me"),
     path('portfolios/', views.PortfoliosView.as_view(), name="portfolios"),
     path('skills/', views.SkillsView.as_view(), name="skills"),
+    path('contact/', views.ContactView.as_view(), name="contact"),
 ]
--- hp/views.py
+++ hp/views.py
@@ -9,8 +9,8 @@ class HomeView(generic.TemplateView):
     template_name = 'home.html'
 
 
-class SelfIntroductionView(generic.TemplateView):
-    template_name = 'self_introduction.html'
+class AboutMeView(generic.TemplateView):
+    template_name = 'about_me.html'
 
 
 class PortfoliosView(generic.TemplateView):
@@ -19,3 +19,7 @@ class PortfoliosView(generic.TemplateView):
 
 class SkillsView(generic.TemplateView):
     template_name = 'skills.html'
+
+
+class ContactView(generic.TemplateView):
+    template_name = 'contact.html'
