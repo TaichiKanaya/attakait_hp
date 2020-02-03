d3c444e3bcacafd7276b98c9748e93784aaa25ee
Taichi.Kanaya <taichi.kanaya@attakit.com>
2020-02-04 07:36:48 +0900

    セキュリティ設定を戻す

--- attakait_hp/settings.py
+++ attakait_hp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '65psx#^q9ur5$wdajj57!^g9y)a!+@f@x(i-uq06wag5u&dzpm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['attakait-hp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
